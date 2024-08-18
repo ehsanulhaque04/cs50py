5ea9cbce9e3adae9171f14bf44bff88ea959018b
ehsanulhaque04 <ehaque2@buffalo.edu>
2024-08-17 20:53:14 -0400

    if else elif

---
+++ compare.py
@@ -0,0 +1,35 @@
+#This is called Contral Flow
+# It's means we will work on 'If , Elif ,and Else for loop while loop'.
+x= int(input("What is x? "))
+y=int(input("WHat is y? "))
+
+
+"""
+start
+if first statement is true then its end here but is its wrong then
+go to next statement and check if the next statement work or not
+"""
+
+if x<y :
+    print("x is less than y")
+if x>y:
+    print("x is greater then y")
+if x== y:
+    print("x is equal to y")
+
+
+# If with elif
+if x<y :
+    print("x is less than y")
+elif x>y:
+    print("x is greater then y")
+elif x== y:
+    print("x is equal to y")
+# IF and ELIF and ELSE
+if x<y :
+    print("x is less than y")
+elif x>y:
+    print("x is greater then y")
+else:
+    print("x is equal to y")
+
